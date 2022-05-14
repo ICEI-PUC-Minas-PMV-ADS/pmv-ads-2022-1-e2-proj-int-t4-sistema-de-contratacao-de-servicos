129de7c882af73042354615dd6435db9d808be13
marioFernands-Puc <mario.fernandes@sga.pucminas.br>
2022-05-14 16:20:37 -0400

    adicionar o api do projeto

---
+++ src/api-contratos-servicos/api-contratos-servicos/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using api_contratos_servicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_contratos_servicos.Context
+{
+    public class ApplicationDbContext:  DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+
+        public DbSet<Cliente>? clientes { get; set; }
+
+        public DbSet<Fornecedor>? Fornecedores { get; set; }
+    }
+}
---
+++ src/api-contratos-servicos/api-contratos-servicos/Models/Dto/UsuarioDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace api_contratos_servicos.Models.Dto
+{
+    
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public UsuarioDTO(int Id, string Nome, string Email)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+
+        public UsuarioDTO()
+        {
+        }
+
+        public Usuario usuarioDtoToUsuario()
+        {
+            var usuario = new Usuario();
+            usuario.Email = Email;
+            usuario.Nome = Nome;
+            usuario.Id = Id;
+
+            return usuario;
+        }
+    }
+
+
+}
